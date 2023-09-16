2ddf670ec0418822c227c45e3af030fa716a3b67
GersonVG <93221954+GersonVG@users.noreply.github.com>
2023-09-16 11:28:24 -0600

    Segundo Ejercicio.fs

--- Segundo Laboratorio/Segundo Ejercicio.fs
+++ Segundo Laboratorio/Segundo Ejercicio.fs
@@ -1,1 +1,12 @@
+// Define una función llamada sub_cadenas que toma una subcadena y una lista de cadenas como parámetros.
+let sub_cadenas (subcadena:string) (lista:string list) =
+    // Utiliza la función List.filter para filtrar las cadenas en la lista.
+    // La función lambda verifica si cada cadena en la lista contiene la subcadena.
+    lista |> List.filter (fun cadena -> String.contains subcadena cadena)
+
+// Ejemplo de uso de la función sub_cadenas.
+let resultado = sub_cadenas "la" ["la casa"; "el perro"; "pintando la cerca"]
+
+// Imprime las cadenas filtradas.
+printfn "Cadenas filtradas: %A" resultado
 
